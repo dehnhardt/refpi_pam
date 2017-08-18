21a9a2ffc0696467bc8820985ca823e63ebf9291
Holger Dehnhardt <holger@dehnhardt.org>
2017-08-18 22:11:25 +0200

    clean everything

--- buildPam.sh
+++ buildPam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc -fPIC -fno-stack-protector -c src/mypam.c
+gcc -fPIC -fno-stack-protector -c src/refpi_pam.c
 
-sudo ld -x --shared -o /lib/security/mypam.so mypam.o
+sudo ld -x --shared -o /lib/security/refpi_pam.so refpi_pam.o
 
-rm mypam.o
+rm refpi_pam.o
